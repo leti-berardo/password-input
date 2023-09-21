b57c54ca1c41f36f8b78fc999d44898c6e0092f5
Leticia Berardo <leticiaberardo@gmail.com>
2023-09-20 21:29:20 -0300

    test failing

--- README.md
+++ README.md
@@ -17,10 +17,9 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 ## Run Tests
 
 ```bash
-npm run test
+npm run tests
 ```
 
 ## Notes about my decisions
 
 - I choose to use tailwind just to simplify the css, even so, i loved to work with css using sass with BEM
--
--- src/components/password-input/password-input.test.tsx
+++ src/components/password-input/password-input.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import PasswordInput from ".";
 
 describe("PasswordInput component", () => {
-  it("renders with default prop values", () => {
+  it("Renders with default prop values", () => {
     render(<PasswordInput options={{}} />);
 
     // Check if the default values are displayed in the component
@@ -19,7 +19,7 @@ describe("PasswordInput component", () => {
     ).toBeNull();
   });
 
-  it("renders with custom prop values", () => {
+  it("Renders with all custom prop values", () => {
     render(
       <PasswordInput
         options={{
@@ -37,6 +37,70 @@ describe("PasswordInput component", () => {
     ).toBeInTheDocument();
     expect(screen.getByText("Has an uppercase letter")).toBeInTheDocument();
     expect(screen.getByText("Has a number 0-9")).toBeInTheDocument();
+    expect(
+      screen.getByText("Has one or more of these special characters: !@#$%^&*")
+    ).toBeInTheDocument();
+  });
+
+  it("Renders with only uppercase custom prop value", () => {
+    render(
+      <PasswordInput
+        options={{
+          uppercase: true,
+        }}
+      />
+    );
+
+    // Check if the custom uppercase value is displayed in the component
+    expect(
+      screen.getByText("Password must be at least 8 characters long")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Has an uppercase letter")).toBeInTheDocument();
+    expect(screen.queryByText("Has a number 0-9")).toBeNull();
+    expect(
+      screen.queryByText(
+        "Has one or more of these special characters: !@#$%^&*"
+      )
+    ).toBeNull();
+  });
+
+  it("Renders with only number custom prop value", () => {
+    render(
+      <PasswordInput
+        options={{
+          number: true,
+        }}
+      />
+    );
+
+    // Check if the custom number value is displayed in the component
+    expect(
+      screen.getByText("Password must be at least 8 characters long")
+    ).toBeInTheDocument();
+    expect(screen.queryByText("Has an uppercase letter")).toBeNull();
+    expect(screen.getByText("Has a number 0-9")).toBeInTheDocument();
+    expect(
+      screen.queryByText(
+        "Has one or more of these special characters: !@#$%^&*"
+      )
+    ).toBeNull();
+  });
+
+  it("Renders with only special character custom prop value", () => {
+    render(
+      <PasswordInput
+        options={{
+          specialChar: true,
+        }}
+      />
+    );
+
+    // Check if the custom special character value is displayed in the component
+    expect(
+      screen.getByText("Password must be at least 8 characters long")
+    ).toBeInTheDocument();
+    expect(screen.queryByText("Has an uppercase letter")).toBeNull();
+    expect(screen.queryByText("Has a number 0-9")).toBeNull();
     expect(
       screen.getByText("Has one or more of these special characters: !@#$%^&*")
     ).toBeInTheDocument();
